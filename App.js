1c59312f111e4a6c4f89af6efb650072bd9e1728
The-Briel-Deal <gabe@fordltc.net>
2022-03-04 21:41:35 -0500

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import { Tab, Text, TabView } from 'react-native-elements';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+
+  function changePage(tabIndex) {
+    setIndex(tabIndex)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Tab
+        value={index}
+        onChange={changePage}
+        indicatorStyle={{
+          backgroundColor: 'white',
+          height: 3,
+        }}
+        variant="primary"
+      >
+        <Tab.Item
+          title="Input"
+        />
+        <Tab.Item
+          title="Graph"
+        />
+      </Tab>
+
+      <TabView
+        value={index} onChange={changePage} animationType="spring"
+      >
+        <TabView.Item style={{width: '100%'}}>
+          <Text h1>Forms</Text>
+        </TabView.Item>
+        <TabView.Item style={{width: '100%'}}>
+          <Text h1>Graph</Text>
+        </TabView.Item>
+      </TabView>
     </View>
   );
 }
